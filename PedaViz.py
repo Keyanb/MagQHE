fb3951b49d50528ed32c105c4424d5040927625c
Keyanb <keyan@protonmail.ch>
2020-04-08 18:02:04 +0200

    Update PedaViz.py

--- PedaViz.py
+++ PedaViz.py
@@ -17,6 +17,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
 
 ni = 4e15
 M = MagneT.MagneT(density = ni) 
@@ -43,7 +44,8 @@ g = M.gESS()
 # df = pd.DataFrame({'Bfield':B, 'DOS': g[-1,:-1], 'Grand Potential': Om[:-1], 'Magnetization': Mag})
 
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets=[dbc.themes.BOOTSTRAP]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
@@ -65,17 +67,26 @@ app.layout = html.Div(children=[
 
     html.Div([
         dcc.Graph(
-            id='density-graph')]),
+            id='density-graph'),
+        html.H1('Progress bar'),
+    dbc.Progress(id="progress", value=50, striped=True, animated=True)]),
     html.Div([
         dcc.Checklist(
             id = 'gocal',
             options = [
-                {'label': 'Grand potential calculation', 'value': 'Go'}],
+                {'label': 'Grand potential calculation (takes about 30s)', 'value': 'Go'}],
             value = '')
         ]),
     html.Div([
         dcc.Graph(
-            id='GranPot-graph')]),
+            id='GranPot-graph'),
+        dcc.Checklist(
+            id = 'gocal2',
+            options = [
+                {'label': 'Mag Calculation', 'value': 'Go'}],
+            value = ''),
+        dcc.Graph(
+            id='Mag-graph')]),
     html.Div([
         html.Div(id='my-div') ])
         
@@ -112,8 +123,8 @@ def update_graph(nel):
 def update_graph2(val, nel): 
     # ns = M._m/(np.pi*k.hbar**2)*nel 
     nsc  = nel/1002*ni    
-    dfo = pd.DataFrame({'Bfield':M._B, 'GrandPotential': g[0]})
-    Om = 0
+    Om = np.zeros(np.shape(shape(M._B)[0]))
+    dfo = pd.DataFrame({'Bfield':M._B, 'GrandPotential': Om}) 
     if 'Go' in val : 
         M.gESS(ns = nsc)
         Om = M.OmegaC()
@@ -122,6 +133,30 @@ def update_graph2(val, nel):
         'data': [dict(
               x=dfo['Bfield'],
               y=dfo['GrandPotential'],
+                # text=dff[dff['Indicator Name'] == yaxis_column_name]['Country Name'],
+                #customdata=dff[dff['Indicator Name'] == yaxis_column_name]['Country Name'],
+                #mode='markers',
+              marker={
+                  'size': 15,
+                  'opacity': 0.5,
+                  'line': {'width': 0.5, 'color': 'white'}
+            }
+        )]
+    }
+
+@app.callback(
+    dash.dependencies.Output('Mag-graph', 'figure'),
+    [dash.dependencies.Input('gocal2', 'value')])
+def update_graph3(val2): 
+    Mag = np.zeros(np.shape(M._B)[0]-1)
+    dfm = pd.DataFrame({'Bfield':M._B[:-1], 'Magnetization': Mag})
+    if 'Go' in val2 :         
+        Mag = M.MagC()
+        dfm = pd.DataFrame({'Bfield':M._B[:-1], 'Magnetization': Mag})
+    return {
+        'data': [dict(
+              x=dfm['Bfield'],
+              y=dfm['Magnetization'],
                 # text=dff[dff['Indicator Name'] == yaxis_column_name]['Country Name'],
                 #customdata=dff[dff['Indicator Name'] == yaxis_column_name]['Country Name'],
                 #mode='markers',
