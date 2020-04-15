931f641e13e69e3e6fcf293d9cb35cf5c8a23df7
Keyanb <keyan@protonmail.ch>
2020-04-15 16:00:00 +0200

    create app for heroku

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 
-web: gunicorn PedaViz:server
+web: gunicorn app:server
--- app.py
+++ app.py
@@ -23,7 +23,7 @@ import dash_bootstrap_components as dbc
 # external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 external_stylesheets=[dbc.themes.BOOTSTRAP]
 
-# server = app.server
+server = app.server
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
